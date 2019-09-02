0a930ccd874edb3c9fcb612a7d6d13e89184ffab
Mooonside <yifeng-chen@outlook.com>
2019-09-02 22:38:26 +0800

    add comment

--- configs/ssd300_coco.py
+++ configs/ssd300_coco.py
@@ -45,7 +45,7 @@ test_cfg = dict(
 # model training and testing settings
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+data_root = '/home/chenyifeng/.mxnet/datasets/coco/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[1, 1, 1], to_rgb=True)
 data = dict(
     imgs_per_gpu=8,
